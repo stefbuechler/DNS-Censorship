174965157686eaede880bed1a62f8ca8dd5c9cb0
Stefan Büchler <36002004+stefbuechler@users.noreply.github.com>
2018-02-10 19:36:48 +0100

    Creation of the Script
    V1.0 of the Script with the Public-DNS Servers of goolge an form uncensoreddns.org.

---
+++ MakeDNSCensorshipUseless.ps1
@@ -0,0 +1,4 @@
+﻿#This script changes your Domain Name Server Adresses on your client. This way you can access websites wich are censord. Have fun and enjoy your open Internet without censorship.
+#V1.0 Creation of the Script Stefan Buechler 10.02.2018 
+#Hint: Run this script as Administrator
+Set-DnsClientServerAddress -ServerAddresses ("8.8.8.8","8.8.4.4","89.233.43.71","91.239.100.10") -InterfaceAlias *
